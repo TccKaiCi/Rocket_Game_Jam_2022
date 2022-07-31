288691a0546b0b007e108b170c5d2e6f167fb7ff
tamertnt3243 <tienem123.com@gmail.com>
2022-07-31 19:45:07 +0700

    -fix

--- Assets/Scripts/Main OBJ/Controller.cs
+++ Assets/Scripts/Main OBJ/Controller.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
+
 public class Controller : MonoBehaviour
 {
+    private const float MAXRAYCASTDISTANCE = 1000f;
     public Camera camera;
+    public LayerMask interactLayer;
     public float maxUseDistance;
     public LayerMask moveAblePlayer;
     Transform target;
     Transform destination;
+    Stack<Vector3> Proceed = new Stack<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -20,19 +25,22 @@ public class Controller : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.DrawRay(mousePos, Camera.main.transform.forward * 1000, Color.red);
-            if (Physics.Raycast(mousePos, Camera.main.transform.forward * 1000f, out RaycastHit hit))
-            {
-                Debug.Log(hit.collider.name);
-                GameObject cursorObj = hit.collider.gameObject;
-                MoveTrashToBin(cursorObj);
-            }
-           
-       
+            ChooseHandle();
+
         }
 
-        /* Camera.main.ScreenToWorldPoint(Input.mousePosition);*/
+    }
+
+    private void ChooseHandle()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawRay(mousePos, Camera.main.transform.forward * MAXRAYCASTDISTANCE, Color.red);
+        if (Physics.Raycast(mousePos, Camera.main.transform.forward * MAXRAYCASTDISTANCE, out RaycastHit hit))
+        {
+            Debug.Log(hit.collider.name);
+            GameObject cursorObj = hit.collider.gameObject;
+            MoveTrashToBin(cursorObj);
+        }
     }
 
     private void MoveTrashToBin(GameObject cursorObj)
@@ -41,20 +49,29 @@ public class Controller : MonoBehaviour
         if (cursorObj.CompareTag("Trash"))
         {
             target = cursorObj.transform;
-            Debug.Log("target " + target.name);
-
         }
         else
         {
+            if(target!=null)
             destination = cursorObj.transform;
-            Debug.Log("destination " + destination.name);
         }
-        if (cursorObj != null && destination != null)
+        if (target!= null && destination != null)
         {
-            Debug.Log(target.name + " " + destination.name);
-            Debug.Log(destination.transform.position);
-            target.transform.DOMove(destination.transform.position, 3f);
+            MoveTrash();
+            ResetChoose();
         }
     }
 
- }
+    private void MoveTrash()
+    {
+        Vector3 des = destination.transform.position;
+        Transform begin = target;
+        begin.DOMove(des, 1);
+    }
+
+    private void ResetChoose()
+    {
+        destination = null;
+        target = null;
+    }
+}
