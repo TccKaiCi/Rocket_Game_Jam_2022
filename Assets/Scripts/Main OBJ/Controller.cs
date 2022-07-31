166b2b00c94c690538e3ac5c44b972b660436b0c
tamertnt3243 <tienem123.com@gmail.com>
2022-07-31 11:11:34 +0700

    -move trash to bin

---
+++ Assets/Scripts/Main OBJ/Controller.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Controller : MonoBehaviour
+{
+    public Camera camera;
+    public float maxUseDistance;
+    public LayerMask moveAblePlayer;
+    Transform target;
+    Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawRay(mousePos, Camera.main.transform.forward * 1000, Color.red);
+            if (Physics.Raycast(mousePos, Camera.main.transform.forward * 1000f, out RaycastHit hit))
+            {
+                Debug.Log(hit.collider.name);
+                GameObject cursorObj = hit.collider.gameObject;
+                MoveTrashToBin(cursorObj);
+            }
+           
+       
+        }
+
+        /* Camera.main.ScreenToWorldPoint(Input.mousePosition);*/
+    }
+
+    private void MoveTrashToBin(GameObject cursorObj)
+    {
+       
+        if (cursorObj.CompareTag("Trash"))
+        {
+            target = cursorObj.transform;
+            Debug.Log("target " + target.name);
+
+        }
+        else
+        {
+            destination = cursorObj.transform;
+            Debug.Log("destination " + destination.name);
+        }
+        if (cursorObj != null && destination != null)
+        {
+            Debug.Log(target.name + " " + destination.name);
+            MoveTrashToBin();
+        }
+    }
+
+    private void MoveTrashToBin()
+    {
+         DOTween.To(() => target.position, x => target.position = x, new Vector3(destination.position.x, destination.position.y, destination.position.z), 1f);
+    }
+}
--- Assets/Scripts/Main OBJ/Conveyor.cs
+++ Assets/Scripts/Main OBJ/Conveyor.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Conveyor : MonoBehaviour
 {
-    public  Transform StartPoint;
+    public Transform StartPoint;
     [SerializeField] int convayorVerlocity;
     // Start is called before the first frame update
     private void OnCollisionStay(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
         Transition(collision.gameObject);
     }
 
     private void Transition(GameObject Trash)
     {
-        if(Trash.TryGetComponent<Rigidbody>(out Rigidbody component)){
+        if (Trash.TryGetComponent<Rigidbody>(out Rigidbody component))
+        {
             component.velocity = gameObject.transform.right * Time.deltaTime * convayorVerlocity;
-        }  
+        }
     }
 
 }
