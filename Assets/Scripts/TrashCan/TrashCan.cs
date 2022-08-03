5075da19f46c197a658ba8e6777794ab17eeb32d
Tăng Chí Chung <tangchichung1872014@gmail.com>
2022-08-03 21:16:21 +0700

    add mechanic to bin

--- Assets/Scripts/TrashCan/TrashCan.cs
+++ Assets/Scripts/TrashCan/TrashCan.cs
@@ -10,7 +10,7 @@ public abstract class TrashCan : MonoBehaviour
     public int MaxCapacity;
     public int CurrentCapacity;
     public bool IsFull => CurrentCapacity == 0;
-    public void TakeTrash() { if (this.IsFull) { Debug.Log("Trash can is full"); } else { DecreaseCapacity(); } }
+    public void TakeTrash() { if (this.IsFull) { Debug.Log("Trash can is full"); } else { Debug.Log("Trash can Capacity " + CurrentCapacity + ", " + Type); DecreaseCapacity(); } }
     public abstract void TakeTrash(Trash trash);
     private void IncreaseCapacity() => this.CurrentCapacity = Mathf.Clamp(this.CurrentCapacity + 1, 0, this.MaxCapacity);
     private void DecreaseCapacity() => this.CurrentCapacity = Mathf.Clamp(this.CurrentCapacity - 1, 0, this.MaxCapacity);
